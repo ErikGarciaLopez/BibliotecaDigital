bb2077d2f03cd952eef5cf7fd5c29b4803dec565
Erik Garcia <erikpumas999@gmail.com>
2024-12-04 22:31:49 -0600

    init

---
+++ src/test/java/com/biblioteca/erik/ErikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.erik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
